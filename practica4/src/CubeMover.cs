1bfa5ab259a659eb822cf29549c5da951a3a80ad
IgorDragone <91559401+IgorDragone@users.noreply.github.com>
2024-10-20 18:09:03 +0100

    Update CubeMover.cs

--- practica4/src/CubeMover.cs
+++ practica4/src/CubeMover.cs
@@ -1,1 +1,12 @@
+using UnityEngine;
 
+public class CubeMover : MonoBehaviour {
+  public float speed = 5f;
+
+  private void Update() {
+    float vertical = Input.GetAxis("Vertical");
+    float horizontal = Input.GetAxis("Horizontal");
+    Vector3 movement = new Vector3(horizontal, 0, vertical);
+    transform.Translate(movement * speed * Time.deltaTime);
+  }
+}
