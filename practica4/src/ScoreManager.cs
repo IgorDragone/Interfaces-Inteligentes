368bae8171dc08649cf1420d5303269a319cfd95
IgorDragone <91559401+IgorDragone@users.noreply.github.com>
2024-10-21 18:05:54 +0100

    Update ScoreManager.cs

--- practica4/src/ScoreManager.cs
+++ practica4/src/ScoreManager.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour {
   private int playerScore = 0;
+	private int rewardCount = 0;
+	private int rewardTreshold = 100;
+	
+	[SerializeField] private TextMeshProUGUI scoreText;
+	[SerializeField] private TextMeshProUGUI rewardPanel;
+
   private void OnEnable() {
     EggCollecting.OnEggCollected += UpdateScore;
   }
@@ -13,10 +20,19 @@ public class ScoreManager : MonoBehaviour {
   private void UpdateScore(int points) {
     playerScore += points;
 		UpdateScoreUI();
+		if (playerScore >= rewardTreshold) {
+			GiveReward();
+			rewardTreshold += 100;
+		}
     // Debug.Log("Player Score: " + playerScore);
   }
 
 	private void UpdateScoreUI() {
 		scoreText.text = "Score: " + playerScore.ToString();
+	}
+
+	private void GiveReward() {
+		rewardCount++;
+		rewardPanel.text = "Stars: " + rewardCount.ToString();
 	}
 }
