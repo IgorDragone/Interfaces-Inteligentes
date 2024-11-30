73d25b799a7d91a1039f1a09d87e1befab41118a
IgorDragone <91559401+IgorDragone@users.noreply.github.com>
2024-11-30 17:54:01 +0000

    Update 03-TV.cs

--- PracticaCamaraMicro/src/03-TV.cs
+++ PracticaCamaraMicro/src/03-TV.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 public class TV : MonoBehaviour
 {
-    // declaracion de variables: Material, WebCamTexture y String para almacenar el path del directorio dodne almacenaremos las captura
     private WebCamTexture webcamTexture;
     private Renderer screenRenderer;
     private Texture2D photoTexture;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         screenRenderer = GetComponent<Renderer>();
-        //Inicializamos el material al que posteriormente se le asignara cada frame de la camara
          if (WebCamTexture.devices.Length > 0)
         {
             string cameraName = WebCamTexture.devices[0].name;
@@ -27,23 +24,16 @@ public class TV : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKey("s")) {
-            //Asignamos a la mainTexture del material el frame actual de la camara. La captura se inicia con Start()
-            // Si la cámara no está activa, iníciala
             if (!webcamTexture.isPlaying)
             {
                 webcamTexture.Play();
             }
-
-            // Asignar el video de la cámara al material
             screenRenderer.material.mainTexture = webcamTexture;
-
         }
         if (Input.GetKey("p")) {
-             // Detener la captura de la cámara
             if (webcamTexture.isPlaying)
             {
                 webcamTexture.Stop();
@@ -51,8 +41,6 @@ public class TV : MonoBehaviour
             }
         }
         if (Input.GetKey("x")) {
-            //Tomar foto
-            // Tomar una foto y asignarla como textura
             if (webcamTexture.isPlaying)
             {
                 TakePhoto();
@@ -62,16 +50,10 @@ public class TV : MonoBehaviour
 
     private void TakePhoto()
     {
-        // Crear una nueva textura con el tamaño del frame de la cámara
         photoTexture = new Texture2D(webcamTexture.width, webcamTexture.height, TextureFormat.RGB24, false);
-
-        // Copiar los píxeles de la cámara a la textura
         photoTexture.SetPixels(webcamTexture.GetPixels());
         photoTexture.Apply();
-
-        // Asignar la textura de la foto al material
         screenRenderer.material.mainTexture = photoTexture;
-
         Debug.Log("Foto tomada.");
     }
 }
